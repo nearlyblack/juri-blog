a3b38e183f9c3a43a8b3fffd8c5ca39125ebee9b
Benjo <beniamino.tartarini@gmail.com>
2020-08-23 18:56:10 +0900

    reset netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
 
   plugins: [
     `gatsby-plugin-react-helmet`,
-    //`gatsby-plugin-netlify`,
+    `gatsby-plugin-netlify`,
 
     {
       resolve: `gatsby-plugin-react-intl`,
